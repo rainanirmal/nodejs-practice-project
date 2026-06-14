89d80d552baafe59c9d0ae6ce4117fc25d3247d4
rainanirmal <rainanirmal01@gmail.com>
2026-06-14 15:38:47 +0530

    feat : add URL parsing and dynamic request handling

--- URL_Module/index.js
+++ URL_Module/index.js
@@ -1,15 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((request , response) => {
+    if(request.url === "/favicon.ico") return response.end();
+
     const log = `${Date.now()} : ${request.url} : New request received\n`;
+
+    const myUrl = url.parse(request.url , true);
+    console.log(myUrl);
+
     fs.appendFile('./log.txt' , log , (err , data) => {
-        switch(request.url) {
+        switch(myUrl.pathname) {
             case '/' :
-                response.end("Hello from server");
+                response.end(`Hello , ${myUrl.query.name}`);
                 break;
             case '/about' :
                 response.end("Hey there , this is Raina Nirmal !");
+                break;
+            case '/search' :
+                response.end("Here are your results for " + myUrl.query.search_query );
                 break;
             default :
                 response.end("404 : Page not found");
