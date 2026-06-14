6d6931d54c2ce1bdb0df37d9a5dae499d5b7049c
rainanirmal <rainanirmal01@gmail.com>
2026-06-14 14:57:53 +0530

    feat : add request logging using fs appendFile

--- URL_Module/index.js
+++ URL_Module/index.js
@@ -1,7 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((request , response) => {
-    response.end("Hello from server");
+    const log = `${Date.now()} : ${request.url} : New request received\n`;
+    fs.appendFile('./log.txt' , log , (err , data) => {
+        switch(request.url) {
+            case '/' :
+                response.end("Hello from server");
+                break;
+            case '/about' :
+                response.end("Hey there , this is Raina Nirmal !");
+                break;
+            default :
+                response.end("404 : Page not found");
+        }
+    })
 });
 
 myServer.listen(8000 , () => {
