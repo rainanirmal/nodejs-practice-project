6524dacab30cb995172ad5529563440de6fbbdaf
rainanirmal <rainanirmal01@gmail.com>
2026-06-15 20:30:51 +0530

    chore: add placeholder routes for POST PATCH and DELETE operations

--- Rest_API_Learning/index.js
+++ Rest_API_Learning/index.js
@@ -22,7 +22,22 @@ app.get("/api/users/:id" , (request , response) => {
     const id = Number(request.params.id);
     const user = users.find((user) => user.id === id);
     response.json(user);
-})
+});
+
+app.post("api/users" , (request , response) => {
+    // TODO : to create user
+    response.json({status : pending});
+});
+
+app.patch("api/users/:id" , (request , response) => {
+    // TODO : to edit existing user with id
+    response.json({status : pending});
+});
+
+app.delete("api/users/:id" , (request , response) => {
+    // TODO : to delete existing user with id
+    response.json({status : pending});
+});
 
 app.listen(PORT , () => {
     console.log("Server started !");
