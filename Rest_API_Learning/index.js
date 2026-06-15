2e4f5723496e1d1a9365eae392be3a2d6234fb01
rainanirmal <rainanirmal01@gmail.com>
2026-06-15 20:20:15 +0530

    feat: add GET endpoints for user listing and lookup by id

--- Rest_API_Learning/index.js
+++ Rest_API_Learning/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
+
+// Routes
+app.get("/users" , (request , response) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`)}
+    </ul>
+    `;
+    response.send(html);
+});
+
+app.get("/api/users" , (request , response) => {
+    response.json(users);
+});
+
+app.get("/api/users/:id" , (request , response) => {
+    const id = Number(request.params.id);
+    const user = users.find((user) => user.id === id);
+    response.json(user);
+})
 
 app.listen(PORT , () => {
     console.log("Server started !");
