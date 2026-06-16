deca0d56c43af502349801b33e6bda18ec8ce396
rainanirmal <rainanirmal01@gmail.com>
2026-06-16 20:15:44 +0530

    feat : define user schema and model for database operation

--- Rest_API_With_Mongodb/index.js
+++ Rest_API_With_Mongodb/index.js
@@ -1,6 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/user-management')
+.then(() => {
+    console.log("MongoDB connected !");
+})
+.catch((err) => {
+    console.log("MongoDB error : " + err);
+})
+
+const userSchema  = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : true,
+    },
+    last_name : {
+        type : String,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    gender : {
+        type : String,
+    },
+    job_title : {
+        type : String,
+    },
+})
+
+const User = mongoose.model("user" , userSchema);
 
 app.get('/' , (request , response) => {
     response.end("Hello ! Testing ..");
