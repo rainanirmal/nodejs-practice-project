019f17a3a9fa6147a335ac73777b19e53ca3a766
rainanirmal <rainanirmal01@gmail.com>
2026-06-17 14:30:21 +0530

    feat : configure mongoDB connection using separate module

--- URL_Shortner/index.js
+++ URL_Shortner/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const connectToMongoDB = require("./connect");
 
 const app = express();
+
+connectToMongoDB("mongodb://127.0.0.1:27017/url-shortner")
+.then(() => {
+    console.log("MongoDB connected !");
+})
+.catch((err) => {
+    console.log("MongoDB error : " + err);
+})
 
 app.listen(8000 , () => {
     console.log("Server started !");
