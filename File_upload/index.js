204a7ca4da0cb51b4a965d9e2c3010f4c95ced1a
rainanirmal <rainanirmal01@gmail.com>
2026-06-24 19:50:10 +0530

    feat : configure multer middleware for file handling

--- File_upload/index.js
+++ File_upload/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' }) 
 
 const PORT = 8000
 
@@ -8,9 +10,13 @@ const app = express();
 app.set("view engine" , "ejs");
 app.set("views" , path.resolve("./views"));
 
+app.use(express.urlencoded({ extended : false}));
+
 app.get('/' , (request , response) => {
     return response.render("homepage");
 })
+
+app.post('/upload' , upload.single("profileImage") , (request , response) => {});
 
 app.listen(PORT , () => {
     console.log("Server started !");
