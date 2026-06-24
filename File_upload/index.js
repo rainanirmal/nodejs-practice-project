837fd2c026293d6fe9881c18fa04f970c1cf5ce8
rainanirmal <rainanirmal01@gmail.com>
2026-06-24 19:36:02 +0530

    feat : add file upload form

--- File_upload/index.js
+++ File_upload/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = 8000
 
 const app = express();
+
+app.set("view engine" , "ejs");
+app.set("views" , path.resolve("./views"));
+
+app.get('/' , (request , response) => {
+    return response.render("homepage");
+})
 
 app.listen(PORT , () => {
     console.log("Server started !");
