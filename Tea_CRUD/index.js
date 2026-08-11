729045a5f1d9dbfd8f888dd7b98289e9fe49289c
rainanirmal <rainanirmal01@gmail.com>
2026-08-11 17:43:23 +0530

    feat: implement route for adding tea

--- Tea_CRUD/index.js
+++ Tea_CRUD/index.js
@@ -3,6 +3,21 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+let teaData = [];
+let nextId = 1;
+
+app.post("/teas" , (req, res) => {
+    
+    const { name, price } = req.body;
+    const newTea = {id: nextId++, name, price};
+
+    teaData.push(newTea);
+
+    return res.status(201).send(newTea);
+})
+
 app.listen(port, () => {
     console.log(`Server started on ${port}`);
 });
